0adcf80e92723db1a26bc7b633241eaa0081e674
W4118 <W4118@lab.aca>
2020-01-09 18:50:03 -0500

    Cambios de l primera clase

--- src/api/controllers/login-controller.ts
+++ src/api/controllers/login-controller.ts
@@ -16,6 +16,9 @@ class LoginController implements IControllerBase {
   public initRoutes() {
     this.router.get('/', sessionFalseMiddleware ,this.index)
     this.router.post('/access', sessionFalseMiddleware ,this.access)
+    this.router.get('/reset_password', sessionFalseMiddleware ,this.reset_password)
+    this.router.get('/signac_in', sessionFalseMiddleware ,this.register)
+    this.router.get('/reset', sessionFalseMiddleware ,this.reset)
   }
 
   index = (req: Request, res: Response) => {
@@ -31,6 +34,36 @@ class LoginController implements IControllerBase {
       jss: loadJs([]), 
     }
     res.status(200).render('login/index', locals)
+  }
+
+  register = (req: Request, res: Response) => {
+    let locals = {
+      title: 'Bienvenido',
+      constants: constants,
+      message_color: '',
+      message: '',
+      csss: loadCss([
+        'assets/css/styles',
+        'assets/css/login',
+      ]), 
+      jss: loadJs([]), 
+    }
+    res.status(200).render('login/register', locals)
+  }
+
+  reset_password = (req: Request, res: Response) => {
+    let locals = {
+      title: 'CORREO',
+      constants: constants,
+      message_color: '',
+      message: '',
+      csss: loadCss([
+        'assets/css/styles',
+        'assets/css/login',
+      ]), 
+      jss: loadJs([]), 
+    }
+    res.status(200).render('login/reset_password', locals)
   }
 
   access = (req: Request, res: Response) => {
@@ -53,6 +86,51 @@ class LoginController implements IControllerBase {
       res.status(200).render('login/index', locals)
     }
   }
+
+  reset = (req: Request, res: Response) => {
+    let mail = req.body.mail
+    let mails = [
+      'pepe@ulima.edu.pe',
+      'hernan@ulima.edu.pe',
+      'jorge@ulima.edu.pe',
+      'lenin@ulima.edu.pe'
+    ]
+    let exist:boolean = false
+    mails.forEach(function (temp){
+      if(temp == mail){
+        exist = true
+      }
+    });
+
+
+    if(exist){
+      let locals = {
+        title: 'Bienvenido',
+        constants: constants,
+        message_color: 'text-succes',
+        message: 'Se ha enviado un correo pra cambiar su contraseña',
+        csss: loadCss([
+          'assets/css/styles',
+          'assets/css/login',
+        ]), 
+        jss: loadJs([]), 
+      }
+    }else{
+      let locals = {
+        title: 'Bienvenido',
+        constants: constants,
+        message_color: 'text-danger',
+        message: 'Correo no registrado',
+        csss: loadCss([
+          'assets/css/styles',
+          'assets/css/login',
+        ]), 
+        jss: loadJs([]), 
+      }
+      res.status(200).render('login/index', locals)
+    }
+  }
 }
+
 
 export default LoginController
